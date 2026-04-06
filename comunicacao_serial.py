72037a446d15a5e835ff5e3005a23c9d470fe2f6
Gustavo Souza da Silva <149391594+Gustavo-silva05@users.noreply.github.com>
2026-04-06 19:01:41 -0300

    Refactor serial communication logic and add close function
    Refactor serial communication functions to improve response handling and add input buffer reset.

--- comunicacao_serial.py
+++ comunicacao_serial.py
@@ -9,7 +9,6 @@ else:
 
 ser = serial.Serial(port, 9600)
 time.sleep(2)
-start = False
 
 def send_and_receive(cmd, delay=0.6):
     ser.write(cmd)
@@ -22,39 +21,38 @@ def serial_communication(cmd, baudrate , data=None):
     try:
         if cmd == "S":
             response = send_and_receive(b'S')
-            start = not start
             if response == "OK":
-                print("Comunicação iniciada.")
+                return "Comunicação iniciada."
             else:
-                print(f"Resposta inesperada: {response}")
+                return(f"Resposta inesperada: {response}")
 
         elif cmd == "T":
-            print(send_and_receive(b'T'))
+            return(send_and_receive(b'T'))
         elif cmd == "H":
-            print(send_and_receive(b'H'))
+            return(send_and_receive(b'H'))
         elif cmd == "TH":
-            print(send_and_receive(b'TH'))
+            return(send_and_receive(b'TH'))
         elif cmd == "C":
-            ser.write(b'C')
-            raw = input("Hex bytes (ex: 01 03 00 00 00 01): ").strip()
-            ser.write((raw + '\n').encode())
+            ser.reset_input_buffer()
+            ser.write(('C' + data + '\n').encode())
             time.sleep(2.0)
             if ser.in_waiting > 0:
-                print(ser.readline().decode('utf-8').rstrip())
+                return(ser.readline().decode('utf-8').rstrip())
             else:
-                print("Sem resposta")
+                return("Sem resposta")
         elif cmd == "B":
-            ser.write(b'B')
-            ser.write((baudrate + '\n').encode())
+            ser.reset_input_buffer()
+            ser.write(('B' + str(baudrate) + '\n').encode())
             time.sleep(0.5)
             if ser.in_waiting > 0:
-                print(ser.readline().decode('utf-8').rstrip())
+                return(ser.readline().decode('utf-8').rstrip())
             ser.baudrate = int(baudrate)
         else:
-            print("Entrada inválida. Use 'T', 'B', 'H', 'C' ou 'S'.")
+            return("Entrada inválida. Use 'T', 'B', 'H', 'C' ou 'S'.")
 
     except KeyboardInterrupt:
         print("\nEncerrando...")
 
-    finally:
-        ser.close()
+    
+def close():
+    ser.close()
